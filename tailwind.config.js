578c9c4f4d1192530095a3a84b541530355a0dac
Lucky Estrada <estrada.lnp@gmail.com>
2025-03-16 01:40:42 +0800

    Major Changes
    Added animate on scroll and improved responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "nunito": ['Nunito', 'sans-serif']
+    }, colors: {
+      black: "#2D3B75",
+      primary: "#F4C2C2",
+      "primary-text-color": "#DF9595",
+      accent: "#FFF3E8",
+      
+    },
+    },
   },
   plugins: [],
 };
